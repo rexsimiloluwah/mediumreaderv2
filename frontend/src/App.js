20797377af9771364ed68ca0b27b4b0a8a152eb1
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-02-15 02:22:19 +0000

    #6 Completed frontend debugging

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './pages/Main';
+import {ToastContainer} from 'react-toastify';
+
+export default function App(){
+  return(
+    <>
+      <ToastContainer />
+      <Main />
+    </>
+  )
+}
---
+++ frontend/src/utils/axiosBase.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const axiosBase = axios.create({
+    baseURL : "http://localhost:5000/api/v1"
+})
+
